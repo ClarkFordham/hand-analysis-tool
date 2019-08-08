6976fbfbd5a594d7c45345c5d92c90bf5388c64f
Clark Fordham <clarkfordham@gmail.com>
2019-08-07 19:14:09 -0700

    file reading

--- hand-analysis-tool/src/main/java/com/clarkfordham/parse.java
+++ hand-analysis-tool/src/main/java/com/clarkfordham/parse.java
@@ -1,13 +1,17 @@
 package com.clarkfordham;
 
+import java.util.*;
 import java.nio.file.*;
+import java.nio.charset.StandardCharsets;
 
 //purpose is to take a text file representing an America's cardrrom (6-max cash game for now) hand history, and turn it into a hand object. 
 class Parse{
 
-    Path filePath;
+    List<String> fileLines;
 
-    Parse(Path path){
+    String filePath;
+
+    Parse(String path){
         filePath = path;
     }
 
@@ -16,11 +20,22 @@ class Parse{
         filePath = null;
     }
 
-    //creates the list of players to be added to the hand
-    Player[] createPlayers(){
-        
-        //
+    //inserts the file into the fileLines variable
+    void readFile(){
+        try{
+            fileLines = Files.readAllLines(Paths.get(filePath), StandardCharsets.UTF_8);
+        } catch (Exception e) {
+            e.printStackTrace();
+            //do something to alert the user that they need to input a file path
+        }
+    }
 
+    Hand buildHand(){
+
+
+
+        //int num, String game, String table, int P, String bigBlind, Player[] player, int button
+        //return new Hand(id, game, table, numPlayers, BB, playerList, buttonNum);
         return null;
     }
 }
